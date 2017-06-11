0c46c765bae2622383831af19b06aad38366ad74
Chris Lam <chris@knetgb.com>
2017-06-11 23:58:44 +0800

    Display newly created pets that match customer preferences

--- public/main.js
+++ public/main.js
@@ -11,6 +11,19 @@
         socket.on('hello', function(data) {
             console.log(data);
         });
+
+        var $list = $('#petsList');
+        socket.on('pet.matched', function(data) {
+            var $innerList = $('<ul></ul>');
+
+            ['name', 'age', 'species', 'breed'].forEach(function (key) {
+                $('<li></li>').html(key + ': ' + data.pet[key]).appendTo($innerList);
+            });
+
+            $list.children('li.nothing').remove();
+
+            $('<li></li>').append($innerList).appendTo($list);
+        });
     }
 })(jQuery);
 
