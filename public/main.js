b1f3903006d34ba6cf4752a85b0927b4b7804b15
Chris Lam <chris@knetgb.com>
2017-06-11 23:51:29 +0800

    Emit watch start event along with customer ID extracted from query string

---
+++ public/main.js
@@ -0,0 +1,22 @@
+(function($){
+    var socket = io();
+
+    var queries = getQueryParams();
+
+    if (typeof queries.customer_id !== 'undefined') {
+        socket.emit('user.watch.start', {
+            customer_id: parseInt(queries.customer_id)
+        });
+    }
+})(jQuery);
+
+function getQueryParams() {
+    var queries = {};
+
+    $.each(document.location.search.substr(1).split('&'),function(c,q){
+        var i = q.split('=');
+        queries[i[0].toString()] = i[1].toString();
+    });
+
+    return queries;
+}
