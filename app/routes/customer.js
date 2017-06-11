0c8c9a139e1f2d11320b5e2c1794e98277bc940a
Chris Lam <chris@knetgb.com>
2017-06-11 15:40:13 +0800

    Create customer together with preference

--- app/routes/customer.js
+++ app/routes/customer.js
@@ -3,14 +3,14 @@ const models = require('../models');
 module.exports = {
     getAll(req, res) {
         models.Customer.findAll({
-            include: [models.CustomerPreference]
+            include: models.CustomerPreference
         }).then((customers) => {
             res.send(customers);
         });
     },
     getOne(req, res) {
         models.Customer.findById(req.params.customer_id, {
-            include: [models.CustomerPreference]
+            include: models.CustomerPreference
         }).then((customer) => {
             if (!customer) {
                 res.sendStatus(404);
@@ -20,9 +20,15 @@ module.exports = {
         });
     },
     post(req, res) {
-        models.Customer.create({
-            name: req.body.name
-        }).then((customer) => {
+        models.Customer.create(
+            {
+                name: req.body.name,
+                customer_preference: req.body.customer_preference || {}
+            },
+            {
+                include: models.CustomerPreference
+            }
+        ).then((customer) => {
             res.status(201).send(customer);
         }).catch((err) => {
             res.status(400).send(err);
