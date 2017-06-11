e21ebde5e06ccde13b0e3e9c26d6fd8cd6ac24a1
Chris Lam <chris@knetgb.com>
2017-06-11 13:56:22 +0800

    Add test case for GET /pets endpoint

--- test/routes/pets.js
+++ test/routes/pets.js
@@ -7,9 +7,17 @@ chai.use(chaiHttp);
 const app = require('../../app');
 
 describe('Pets', () => {
-    describe('GET /pets)', () => {
+    describe('GET /pets', () => {
         it('it should GET all the pets', (done) => {
+            chai.request(app)
+                .get('/pets')
+                .end((err, res) => {
+                    assert.equal(res.status, 200);
+                    assert.isArray(res.body, 'array');
+                    assert.isNotEmpty(res.body);
 
+                    done();
+                });
         });
     });
 });
