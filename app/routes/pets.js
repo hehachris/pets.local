22905241f4ac976fa19bb02cdeaaac759cd8f643
Chris Lam <chris@knetgb.com>
2017-06-11 14:18:38 +0800

    Add route to fetch one pet

--- app/routes/pets.js
+++ app/routes/pets.js
@@ -5,5 +5,14 @@ module.exports = {
         models.Pet.findAll().then((pets) => {
             res.send(pets);
         });
+    },
+    getOne(req, res) {
+        models.Pet.findById(req.params.pet_id).then((pet) => {
+            if (!pet) {
+                res.sendStatus(404);
+            } else {
+                res.send(pet);
+            }
+        });
     }
 };
--- config/routes.js
+++ config/routes.js
@@ -2,4 +2,5 @@ const pets = require('../app/routes/pets');
 
 module.exports = (app) => {
     app.get('/pets', pets.getAll);
+    app.get('/pets/:pet_id', pets.getOne);
 };
