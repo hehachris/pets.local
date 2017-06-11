45c7d666564aa410047f7964c4e12a426ea42a91
Chris Lam <chris@knetgb.com>
2017-06-11 23:56:55 +0800

    Store connected socket in req object indexed by customer id

--- app/libs/sockets.js
+++ app/libs/sockets.js
@@ -1,7 +1,16 @@
 module.exports = (app, io) => {
+    const sockets = {};
+
+    app.use((req, res, next) => {
+        req.sockets = sockets;
+        next();
+    });
+
     io.on('connection', (socket) => {
         socket.on('user.watch.start', (data) => {
             const customerId = parseInt(data.customer_id, 10);
+
+            sockets[customerId] = socket;
 
             socket.emit('hello', {
                 customer_id: customerId
