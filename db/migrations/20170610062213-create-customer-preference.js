166a39cdb84525fe36ebe53a2f40ca5c902107e5
Chris Lam <chris@knetgb.com>
2017-06-12 22:30:09 +0800

    Update migration file

--- db/migrations/20170610062213-create-customer-preference.js
+++ db/migrations/20170610062213-create-customer-preference.js
@@ -22,11 +22,11 @@ module.exports = {
                 allowNull: true
             },
             species: {
-                type: Sequelize.ENUM('cat', 'dog', 'rabbit'),
+                type: Sequelize.STRING,
                 allowNull: true
             },
             breed: { // For dogs only
-                type: Sequelize.ENUM('labrador', 'poodle', 'spaniel', 'terrier'),
+                type: Sequelize.STRING,
                 allowNull: true
             }
         });
