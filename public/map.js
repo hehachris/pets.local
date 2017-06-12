22476bd5a80408911a5481f9a0919327d789c9ff
Chris Lam <chris@knetgb.com>
2017-06-12 20:47:41 +0800

    Show nearby pets as markers

--- public/map.js
+++ public/map.js
@@ -1,16 +1,47 @@
 var map;
+var petMarkers = []
 
 function onCurrentPosition(startingPosition) {
     map = new google.maps.Map(document.getElementById('map'), {
         center: {
+            //lat: 22.35622784,
+            //lng: 114.11518335
             lat: startingPosition.coords.latitude,
             lng: startingPosition.coords.longitude
         },
         zoom: 15
     });
 
-    var startingMarker = new google.maps.Marker({
-        position: map.getCenter(),
+    map.addListener('dragend', showNearbyPets);
+}
+
+function displayPetMarker(lat, lng) {
+    var marker = new google.maps.Marker({
+        position: {
+            lat: lat,
+            lng: lng
+        },
         map: map
+    });
+
+    petMarkers.push(marker);
+}
+
+function showNearbyPets(km) {
+    var center = map.getCenter();
+
+    $.ajax({
+        url: '/pets',
+        data: {
+            latitude: center.lat,
+            longitude: center.lng,
+            km: km || 2
+        },
+        success: function(data) {
+            data.forEach(function(pet) {
+                displayPetMarker(pet.latitude, pet.longitude);
+            });
+        },
+        dataType: 'json'
     });
 }
